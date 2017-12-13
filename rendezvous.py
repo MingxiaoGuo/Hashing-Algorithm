a414df3efb9a31ad86d3911cf334891e4f9f7963
MingxiaoGuo <mguo@paloaltonetworks.com>
2017-12-12 17:15:23 -0800

    remove somethign

--- rendezvous.py
+++ rendezvous.py
@@ -4,22 +4,11 @@ class RendezvousHash(object):
     """This class implements the Rendezvous (HRW) hashing logic."""
 
     def __init__(self, nodes=None):
-        """
-        Initialize the instance with node list and others.
-        A node means a server host name and its listening port. E.g. '0.0.0.0:3000' 
-        :param nodes: a list of DB server nodes to register.
-        """
         # TODO
         self.nodes = nodes
         
     
     def get_node(self, key):
-        """
-        Find the highest hash value via hash(node+key) and the node that generates the highest
-        value among all nodes. Encode string to utf-8 if the hash function needs it.
-        :param key: a string key name.
-        :return the highest node. E.g. '0.0.0.0:3000'
-        """
         # TODO
         highest_node = None
         hash_result = {}
